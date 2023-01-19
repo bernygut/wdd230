a70a84480e7c1364663961268152ac73303353eb
bernygut <bernygutierrez@gmail.com>
2023-01-18 23:26:02 -0600

    added script

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+// Select the HTML element to manipulate
+const date1 = document.querySelector("#date1");
+let oLastModif = new Date(document.lastModified);
+date1.innerHTML = `Last Updated: ${oLastModif}`;
+
