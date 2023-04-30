79bc2becd61cd5248f323e752f18b90dcbb42747
bernygut <bernygutierrez@gmail.com>
2023-04-29 23:24:26 -0600

    Fix of date

--- scripts/script.js
+++ scripts/script.js
@@ -4,5 +4,5 @@ let oLastModif = new document.lastModified;
 date1.innerHTML = `Last Updated: ${oLastModif}`;
 
 const year = document.querySelector("#year");
-let thisHappyYear = new Date(document.lastModified).getFullYear;
-year.innerHTML = `© ${thisHappyYear} | Berny Gutiérrez | Santa Ana, San José, Costa Rica`;
+let thisYear = getFullYear(document.lastModified);
+year.innerHTML = `© ${thisYear} | Berny Gutiérrez | Santa Ana, San José, Costa Rica`;
