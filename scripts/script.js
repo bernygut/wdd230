9f14bbd606a9ba0fc1ef8fa31ae966724642bf02
bernygut <bernygutierrez@gmail.com>
2023-04-29 23:03:27 -0600

    Updates to fix copyright message

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@
 const date1 = document.querySelector("#date1");
 let oLastModif = new document.lastModified;
 date1.innerHTML = `Last Updated: ${oLastModif}`;
-const year = document.querySelector("year");
-let thisYear = new Date(document.lastModified).getFullYear;
-year.innerHTML = "© ${thisYear} | Berny Gutiérrez | Santa Ana, San José, Costa Rica";
+
+const year = document.querySelector("#year");
+let thisHappyYear = new Date(document.lastModified).getFullYear;
+year.innerHTML = `© ${thisHappyYear} | Berny Gutiérrez | Santa Ana, San José, Costa Rica`;
