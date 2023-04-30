67d73e054515ecef7e6b3471e7b6b583adc99967
bernygut <bernygutierrez@gmail.com>
2023-04-29 22:52:10 -0600

    Updated for dynamic copyright year

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 // Select the HTML element to manipulate
 const date1 = document.querySelector("#date1");
-let oLastModif = new Date(document.lastModified);
+let oLastModif = new document.lastModified;
 date1.innerHTML = `Last Updated: ${oLastModif}`;
+const year = document.querySelector("year");
+let thisYear = new Date(document.lastModified).getFullYear;
+year.innerHTML = "© ${thisYear} | Berny Gutiérrez | Santa Ana, San José, Costa Rica";
