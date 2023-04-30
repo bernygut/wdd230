ca2bee88fef2e0abb8d97d723ca37104f65a3627
bernygut <bernygutierrez@gmail.com>
2023-04-29 23:38:48 -0600

    Another update to scripts for errors

--- scripts/script.js
+++ scripts/script.js
@@ -4,5 +4,6 @@ let oLastModif = document.lastModified;
 date1.innerHTML = `Last Updated: ${oLastModif}`;
 
 const year = document.querySelector("#year");
-let thisYear = getFullYear(document.lastModified);
+const thisDate = new Date();
+const thisYear = thisDate.getFullYear();
 year.innerHTML = `© ${thisYear} | Berny Gutiérrez | Santa Ana, San José, Costa Rica`;
