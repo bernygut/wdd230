ceeb8e4efc8ce8e495c0b15dc388418292006f79
bernygut <bernygutierrez@gmail.com>
2023-05-03 23:38:29 -0600

    Updates to Design Principles

--- lesson2/scripts/script.js
+++ lesson2/scripts/script.js
@@ -1,5 +1,8 @@
-// Select the HTML element to manipulate
 const date1 = document.querySelector("#date1");
-let oLastModif = new Date(document.lastModified);
+let oLastModif = document.lastModified;
 date1.innerHTML = `Last Updated: ${oLastModif}`;
-//Last updated for lesson2
+
+const year = document.querySelector("#year");
+const thisDate = new Date();
+const thisYear = thisDate.getFullYear();
+year.innerHTML = `© ${thisYear} | Berny Gutiérrez | Santa Ana, San José, Costa Rica`;
